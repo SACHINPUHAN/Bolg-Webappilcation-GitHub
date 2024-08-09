2b02873bfceffa887e7adc18a62f7dc9b715a447
SACHINPUHAN <sachinpuhan65@gmail.com>
2024-08-09 19:03:20 +0530

    1
    first

---
+++ blogapp/blog/src/main/java/com/bloggingbackend/BloggingappapiApplication.java
@@ -0,0 +1,13 @@
+package com.bloggingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingappapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingappapiApplication.class, args);
+	}
+
+}
